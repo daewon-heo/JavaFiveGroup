549d613410845a6c8c855c1df196486187c54ddd
pockjw <42015357+pockjw@users.noreply.github.com>
2018-08-09 14:36:23 +0900

    오늘 끝

--- src/com/ff/controller/TodayStyleController.java
+++ src/com/ff/controller/TodayStyleController.java
@@ -1,6 +1,11 @@
 package com.ff.controller;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 import com.ff.view.TodayStyleView;
+
+import develop.Weather;
 
 public class TodayStyleController {
 	TodayStyleView TodayStyleView = null;
@@ -10,6 +15,45 @@ public class TodayStyleController {
 	}
 	
 	public void viewShow() {
-		TodayStyleView = new TodayStyleView();
+		TodayStyleView = new TodayStyleView(randStyle());
+//		String[][] datas = Weather.GetPastWeather("108", 2018, "08");
+//		TodayStyleView.dataView(datas);
+	}
+	
+	public String randStyle(){
+		Calendar cal = new GregorianCalendar();
+		int iSeason = cal.get(Calendar.DAY_OF_MONTH) -1;
+		System.out.println("현재 월 : " + iSeason);
+		
+		String sSeason = null;
+		
+		switch (iSeason) {
+		case 12:
+		case 1:
+		case 2:
+			sSeason = "winter";
+			break;
+		case 3:
+		case 4:
+		case 5:
+			sSeason = "spring";
+			break;
+		case 6:
+		case 7:
+		case 8:
+			sSeason = "summer";
+			break;
+		case 9:
+		case 10:
+		case 11:
+			sSeason = "fall";
+			break;
+		default:
+			break;
+		}
+		String stylePath = "datas/images/style/" + sSeason +  "/" + String.valueOf((int)(Math.random()*9)+1).substring(0,1) + ".jpg";
+		
+		return stylePath;
+		
 	}
 }
--- src/com/ff/view/TodayStyleView.java
+++ src/com/ff/view/TodayStyleView.java
@@ -1,8 +1,17 @@
 package com.ff.view;
 
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class TodayStyleView extends JFrame {
 
@@ -11,16 +20,64 @@ public class TodayStyleView extends JFrame {
 	 */
 	private static final long serialVersionUID = 181983372302562901L;
 	
-	public TodayStyleView(){
-		super("두번째 화면");
-		initView();
+	public TodayStyleView(String fileName){
+		super("이런 스타일 어때요?");
+		initView(fileName);
+		System.out.println(fileName);
 	}
 	
-	public void initView() {
-		setBounds(300, 300, 500, 500);
+	public void dataView(String[][] datas){
+//		JLabel label = new JLabel(datas[3][3]);
+//		add(label);
+	}
+	
+	public void initView(String fileName) {
+		
+		setBounds(500, 200, 500, 500);
+		setSize(450, 800);
+		setLocationRelativeTo(null);
 		setLayout(new GridLayout());
 //		setResizable(false);
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+		JPanel pan = new JPanel();
+		JLabel lb = new JLabel(new ImageIcon(fileName));
+		lb.setLocation(0, 0);
+		lb.setSize(450, 800);
+//	
+		pan.add(lb);
+		add(pan);
+		
+		pan.setLayout(null);
+		
+		JButton btn = new JButton(new ImageIcon("datas/images/showmorepicbtn.png"));
+		btn.setLocation(290, 710);
+		btn.setSize(130, 35);
+	
+		pan.add(btn);
+		
+		btn.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
+		
+		
+		
+		try {
+			setIconImage(ImageIO.read(new File("datas/images/rainbow.png")));
+			
+
+		} catch (IOException e) {
+		
+			System.out.println("이미지 읽기 실패!");
+		}
+		
 	}
+	
 }
