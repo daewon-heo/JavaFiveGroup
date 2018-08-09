ed657235c065de2de179d2882f08a6ff2e5167ba
daewon-heo <eo7655@naver.com>
2018-08-09 15:08:49 +0900

    목록 설명 추가

--- src/develop/Run.java
+++ src/develop/Run.java
@@ -5,27 +5,29 @@ import java.util.Map;
 public class Run {
 
 	public static void main(String[] args) {
-		String stn = "108";
 		int year = 2018;
 		String obs = "21";
-		Map<String, String> awsMap = Weather.GetCurrentWeather(stn);
-		System.out.println(awsMap);
-		System.out.println("현재 기온 : " + awsMap.get("기온"));
-		
-		String[][] pastWeather = null;
-		pastWeather = Weather.GetPastWeather(stn, year, obs);
-
-		if (pastWeather != null) {
-
-			for (int i = 0; i < pastWeather.length; i++) {
-				for (int j = 0; j < pastWeather[0].length; j++) {
-					System.out.print(pastWeather[i][j] + "\t");
-				}
-				System.out.println();
-			}
-		}else {
-			System.out.println("파라미터를 확인하세요");
-		}
-		System.out.println("프로그램 종료");
+//		Map<String, String> awsMap = Weather.GetCurrentWeather(stn);
+//		System.out.println(awsMap);
+//		System.out.println("현재 기온 : " + awsMap.get("기온"));
+//		System.out.println("강수감지 : " + awsMap.get("강수감지"));
+//		
+//		String[][] pastWeather = null;
+//		pastWeather = Weather.GetPastWeather(stn, year, obs);
+//
+//		if (pastWeather != null) {
+//
+//			for (int i = 0; i < pastWeather.length; i++) {
+//				for (int j = 0; j < pastWeather[0].length; j++) {
+//					System.out.print(pastWeather[i][j] + "\t");
+//				}
+//				System.out.println();
+//			}
+//		}else {
+//			System.out.println("파라미터를 확인하세요");
+//		}
+//		
+//		System.out.println("오늘 : " + pastWeather[9][8]);
+//		System.out.println("프로그램 종료");
 	}
 }
