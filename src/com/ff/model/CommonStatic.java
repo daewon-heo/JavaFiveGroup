dcbc5dc57ebd5413ecda3db4927461ae614cb970
daewon-heo <eo7655@naver.com>
2018-08-09 13:01:25 +0900

    common static String 정의

---
+++ src/com/ff/model/CommonStatic.java
@@ -0,0 +1,8 @@
+package com.ff.model;
+
+public class CommonStatic {
+	/* IMG */
+	public static String LOADING_IMG_WEATHER = "datas/images/loading.gif";
+	public static String BACKGROUND_SKY_IMG = "datas/images/skyBG.jpg";
+
+}
