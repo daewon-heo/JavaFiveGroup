fd5e82208f2ae829bf6588a051866337a702b8d7
daewon-heo <eo7655@naver.com>
2018-08-09 09:09:00 +0900

    에러 수정

--- src/com/ff/run/Run.java
+++ src/com/ff/run/Run.java
@@ -7,7 +7,7 @@ import develop.Develop;
 public class Run {
 
 	public static void main(String[] args) {
-		new  MainController().showView();
+		new  MainController().viewShow();
 //		System.out.println("첫번째 팀 프로젝트");
 //		new Develop().test();
 	}
